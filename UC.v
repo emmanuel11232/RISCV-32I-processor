ff4aa6ca9fc915ea1e6e52040153c5f8b4fe4218
emmanuel11232 <103795469+emmanuel11232@users.noreply.github.com>
2023-05-17 13:29:45 -0600

    Unidad de control con testbench
    Se agrega la unidad de control encargada de señales de control para cada módulo junto con su testbench

---
+++ UC.v
@@ -0,0 +1,126 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11.05.2023 22:27:03
+// Design Name: 
+// Module Name: UC
+// Project Name: Proyecto RISC-V
+// Target Devices: 
+// Tool Versions: 
+// Description: Este módulo toma los bits del [6:2] de la instrucción (esto porque los bits del [0:1] siempre son 11 para todas las instrucciones)
+//              y genera las señales de control apropiadas para cada caso.
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module UC(
+  //Se definen las entradas y salidas del módulo
+  input [4:0] selector,
+  input Zero,
+
+  output reg Branch,
+  output reg Jump,
+  output reg PCSrc,
+  output reg [1:0] ImmSel,
+  output reg LUIOP,
+  output reg WDSrc,
+  output reg ALUSrc,
+  output reg ALUOP,
+  output reg Mem2Reg,
+  output reg MemWrite,
+  output reg RegWriteEn
+);
+//Se colocan las salidas según la tabla de verdad diseñada para cada caso
+always @(*)
+  begin
+    case (selector)
+      5'b00000: begin
+        Branch = 0;
+        Jump = 0;
+        ImmSel = 2'b00;
+        LUIOP = 0;
+        WDSrc = 0;
+        ALUSrc = 1;
+        ALUOP = 0;
+        Mem2Reg = 0;
+        MemWrite = 0;
+        RegWriteEn = 1;
+        end
+      5'b00100: begin
+        Branch = 0;
+        Jump = 0;
+        ImmSel = 2'b00;
+        LUIOP = 0;
+        WDSrc = 0;
+        ALUSrc = 1;
+        ALUOP = 0;
+        Mem2Reg = 1;
+        MemWrite = 0;
+        RegWriteEn = 1;
+        end
+      5'b01000: begin
+        Branch = 0;
+        Jump = 0;
+        ImmSel = 2'b01;
+        LUIOP = 0;
+        ALUSrc = 1;
+        ALUOP = 0;
+        MemWrite = 1;
+        RegWriteEn = 0;
+        end
+      5'b11000: begin
+        Branch = 1;
+        Jump = 0;
+        ImmSel = 2'b10;
+        LUIOP = 0;
+        ALUSrc = 0;
+        ALUOP = 1;
+        MemWrite = 0;
+        RegWriteEn = 0;
+        end
+      5'b01101: begin
+        Branch = 0;
+        Jump = 0;
+        LUIOP = 1;
+        WDSrc = 1;
+        MemWrite = 0;
+        RegWriteEn = 1;
+        end
+      5'b11011: begin
+        Branch = 0;
+        Jump = 1;
+        ImmSel = 2'b11;
+        LUIOP = 0;
+        WDSrc = 1;
+        ALUOP = 0;
+        MemWrite = 0;
+        RegWriteEn = 1;
+        end
+      default:  begin
+        Branch = 0;
+        Jump = 0;
+        ImmSel = 2'b00;
+        LUIOP = 0;
+        WDSrc = 0;
+        ALUSrc = 0;
+        ALUOP = 0;
+        Mem2Reg = 0;
+        MemWrite = 0;
+        RegWriteEn = 0;
+        end
+    endcase
+  end
+  //Lógica combinacional para el control de PCSource
+  always @(*)
+  begin
+    PCSrc = (Branch & Zero) || Jump;
+  end
+endmodule
---
+++ UC_Testbench.v
@@ -0,0 +1,97 @@
+`timescale 1ns/1ns
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11.05.2023 22:37:05
+// Design Name: 
+// Module Name: UC_Testbench
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module UC_Testbench;
+// Declaración de señales para el testbench
+  reg [4:0] selector;
+  reg Zero;
+  wire Branch, Jump, PCSrc;
+  wire [1:0] ImmSel;
+  wire LUIOP, WDSrc, ALUSrc;
+  wire ALUOP, Mem2Reg, MemWrite, RegWriteEn;
+
+  // Instancia del módulo que se va a probar (ejemplo_case)
+  UC dut(
+    .selector(selector),
+    .Zero(Zero),
+    .Branch(Branch),
+    .Jump(Jump),
+    .PCSrc(PCSrc),
+    .ImmSel(ImmSel),
+    .LUIOP(LUIOP),
+    .WDSrc(WDSrc),
+    .ALUSrc(ALUSrc),
+    .ALUOP(ALUOP),
+    .Mem2Reg(Mem2Reg),
+    .MemWrite(MemWrite),
+    .RegWriteEn(RegWriteEn)
+    );
+    // Estímulos del testbench
+  initial begin
+    // Asignar valores a las señales de entrada
+    selector = 5'b00000;
+    Zero = 0;
+
+    // Esperar un tiempo para estabilizar las señales
+    #10;
+
+    // Cambiar los valores de las señales de entrada
+    selector = 5'b00100;
+    Zero = 0;
+
+    // Esperar un tiempo
+    #10;
+
+    // Otro cambio en las señales de entrada
+    selector = 5'b01000;
+    Zero = 0;
+    // Esperar un tiempo
+    #10;
+    
+    // Otro cambio en las señales de entrada
+    selector = 5'b11000;
+    Zero = 1;
+    // Esperar un tiempo
+    #10;
+    
+    // Otro cambio en las señales de entrada
+    selector = 5'b01101;
+    Zero = 0;
+    // Esperar un tiempo
+    #10;
+    
+    // Otro cambio en las señales de entrada
+    selector = 5'b11011;
+    Zero = 0;
+
+    // Esperar un tiempo
+    #10;
+
+    // Finalizar la simulación
+    $finish;
+  end
+
+  // Impresión de resultados
+  always @*
+  begin
+    $display("selector: %b, Zero: %b, Branch: %b, Jump: %b, PCSrc: %b, ImmSel: %b, LUIOP: %b, WDSrc: %b, ALUSrc: %b, ALUOP: %b, Mem2Reg: %b, MemWrite: %b, RegWriteEn: %b", selector, Zero, Branch, Jump, PCSrc, ImmSel, LUIOP, WDSrc, ALUSrc, ALUOP, Mem2Reg, MemWrite, RegWriteEn);
+  end
+endmodule
