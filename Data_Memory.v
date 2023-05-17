95877046fcbe17565335ae6639615aceddc13171
emmanuel11232 <103795469+emmanuel11232@users.noreply.github.com>
2023-05-17 12:19:02 -0600

    Memoria de datos funcional
    Se incluye la memoria de datos y su respectivo testbench

---
+++ Data_Memory.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/29/2023 10:37:22 PM
+// Design Name: 
+// Module Name: Data_Memory
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Data_Memory(
+   input      [31:0]data,
+   input     [31:0]addr,
+   input we, clk,rst,
+   output     [31:0]q);
+   
+   integer i;
+   localparam Espacio=256;
+   reg        [31:0]ram[Espacio:0];
+   always @ (posedge clk or negedge rst) begin
+        if (!rst) begin
+            for (i = 0; i < Espacio; i = i + 1) begin
+                ram[i] <= 0;
+                end
+          end
+        if (we)
+           ram[addr] <= data;
+        end
+   assign q = ram[addr]; // read with input address directly    
+endmodule
